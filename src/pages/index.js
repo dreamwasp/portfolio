930d159937046ec99eb7b768f77638c3c318adaa
Cassie Spain <ceaspain@Cassies-MacBook-Air.local>
2024-09-22 12:51:46 -0400

    test change text

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,13 +45,8 @@ export default () => {
           </div>
           <br></br>
           <p className={genText.paragraphs}>
-            i attended the grace hopper program @ fullstack academy in 2020 +
-            have had my head in my laptop ever since. when not coding, catch me
-            reading experimental fiction, playing animal crossing, or checking
-            out weird art of all kinds.<br></br>
-            <br></br>experienced in javascript, node.js, postgres, firestore,
-            sequelize, axios, express, react, react native, + redux but learning
-            more all the time.
+            i've spent the last four years working on design systems and frontend infrastructure. when not coding, catch me
+            reading experimental fiction, playing magic the gathering, or checking out weird art of all kinds.
             <br></br>
             <br></br>
             <a href="mailto:cea.spain@gmail.com" className={genText.link}>
