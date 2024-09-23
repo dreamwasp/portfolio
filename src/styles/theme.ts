b8369eb7d0ed02953f2a93d7f225b73d97be609a
Cassie Spain <ceaspain@Cassies-MacBook-Air.local>
2024-09-22 21:38:09 -0400

    theme start

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  background: {
+    header: "#0e0e0e",
+    body: "#121213",
+    footer: "#161618",
+  },
+  colors: {
+    primary: "#afc2d5",
+    contrast: "whitesmoke",
+    secondary: "#6193bc",
+    emphasis: "#00bcf5;",
+    other: "#b5c5ca",
+    subtle: "#484e50",
+  },
+};
